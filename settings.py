5d1ecdc717055d6b5d6fb689fcaf63baef54d4a3
zilohumberto <zilohumberto@gmail.com>
2022-06-03 11:01:05 -0300

    initial commit, setup

---
+++ settings.py
@@ -0,0 +1,5 @@
+from decouple import config
+
+API_ONE = config("API_ONE", default="https://api1.com")
+API_TWO = config("API_TWO", default="https://api2.com")
+API_THREE = config("API_THREE", default="https://api3.com")
